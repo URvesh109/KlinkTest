36be2130b418ae5b936a064b03647769c0889912
Urvesh109 <urveshi.t@gmail.com>
2024-06-19 22:58:35 +0530

    Add extra, thin and medium etc fonts

--- src/assets/fonts.ts
+++ src/assets/fonts.ts
@@ -1,3 +1,11 @@
 export default {
+  Black: 'Inter-Black',
+  Bold: 'Inter-Bold',
+  ExtraBold: 'Inter-ExtraBold',
+  ExtraLight: 'Inter-ExtraLight',
+  Light: 'Inter-Light',
+  Medium: 'Inter-Medium',
   Regular: 'Inter-Regular',
+  SemiBold: 'Inter-SemiBold',
+  Thin: 'Inter-Thin',
 };
