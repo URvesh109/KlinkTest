1641c81789f8b5f7e4c5e53e520a2a7f0c1efb69
Urvesh109 <urveshi.t@gmail.com>
2024-06-21 13:50:12 +0530

    Add refreshControl Component

--- src/homeScreen.tsx
+++ src/homeScreen.tsx
@@ -3,23 +3,39 @@ import {Portfolio} from './components';
 import CryptoActivity from './components/cryptoActivity';
 import WalletHeader from './components/walletHeader';
 import DurationChart from './components/durationChart';
-import {ScrollView} from './theme';
+import {palette, ScrollView} from './theme';
 import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
-import {useRecoilValue, useRecoilState} from 'recoil';
-import {loadingState} from './atoms';
-// import {fetchCoinList} from './apis';
+import {useRecoilValue, useRecoilState, useSetRecoilState} from 'recoil';
+import {loadingState, messageState} from './atoms';
+import {fetchCoinList} from './apis';
+import {RefreshControl, StyleSheet} from 'react-native';
 
 const Home = () => {
   const [isLoading, setLoading] = useRecoilState(loadingState);
+  const setErrorMessage = useSetRecoilState(messageState);
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 4000);
+  };
 
   React.useEffect(() => {
     async function fetch() {
       try {
-        setLoading(true);
-        setTimeout(() => {
-          setLoading(false);
-        }, 3000);
-        // await fetchCoinList();
+        // setErrorMessage({
+        //   visible: true,
+        //   info: 'Something went wrong!',
+        //   type: 'error',
+        // });
+        // setLoading(true);
+        // setTimeout(() => {
+        //   setLoading(false);
+        // }, 3000);
+        // const data = await fetchCoinList();
+        // data.forEach(item => console.log('ids', item.id));
       } catch (error) {
         console.log('Error');
       }
@@ -29,14 +45,29 @@ const Home = () => {
 
   return (
     <BottomSheetModalProvider>
-      <ScrollView nestedScrollEnabled={false}>
+      <ScrollView
+        nestedScrollEnabled={false}
+        refreshControl={
+          <RefreshControl
+            tintColor={palette.white}
+            style={styles.indicatorBackgroundColor}
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }>
         <WalletHeader />
         <DurationChart />
         <CryptoActivity />
-        <Portfolio />
+        {/* <Portfolio /> */}
       </ScrollView>
     </BottomSheetModalProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  indicatorBackgroundColor: {
+    backgroundColor: 'transparent',
+  },
+});
 
 export default Home;
