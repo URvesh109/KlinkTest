0cf331267ec376f150bd416f11f0b2b4ebd4c876
Urvesh109 <urveshi.t@gmail.com>
2024-06-21 22:42:05 +0530

    Refactor chart component

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -2,18 +2,14 @@ import React from 'react';
 import {LineChart, LineChartPropsType} from 'react-native-gifted-charts';
 
 export const Chart: React.FC<LineChartPropsType> = props => {
-  const {color, startFillColor, endFillColor, ...rest} = props;
+  const {...rest} = props;
   return (
     <LineChart
       areaChart
-      data={ptData}
       isAnimated
       animationDuration={1000}
       hideDataPoints
-      color={color}
       thickness={1}
-      startFillColor={startFillColor}
-      endFillColor={endFillColor}
       startOpacity={0.35}
       endOpacity={0.01}
       hideYAxisText
@@ -25,65 +21,3 @@ export const Chart: React.FC<LineChartPropsType> = props => {
     />
   );
 };
-
-const ptData = [
-  {value: 120},
-  {value: 140},
-  {value: 145},
-  {value: 160},
-  {value: 200},
-  {value: 220},
-  {value: 280},
-  {value: 260},
-  {value: 340},
-  {value: 385},
-  {value: 280},
-  {value: 390},
-  {value: 280},
-  {value: 190},
-  {value: 180},
-  {value: 140},
-  {value: 145},
-  {value: 160},
-  {value: 200},
-  {value: 220},
-  {value: 280},
-  {value: 260},
-  {value: 340},
-  {value: 385},
-  {value: 280},
-  {value: 390},
-  {value: 280},
-  {value: 190},
-  {value: 180},
-  {value: 140},
-  {value: 145},
-  {value: 160},
-  {value: 200},
-  {value: 220},
-  {value: 280},
-  {value: 260},
-  {value: 340},
-  {value: 385},
-  {value: 280},
-  {value: 390},
-  {value: 370},
-  {value: 285},
-  {value: 295},
-  {value: 300},
-  {value: 280},
-  {value: 295},
-  {value: 260},
-  {value: 255},
-  {value: 190},
-  {value: 220},
-  {value: 205},
-  {value: 230},
-  {value: 210},
-  {value: 200},
-  {value: 240},
-  {value: 350},
-  {value: 380},
-  {value: 350},
-  {value: 310},
-];
